60ae9fe625e924d59e30a7181a13c017f754befe
lucbon26 <lucasbonfil@gmail.com>
2026-09-08 02:12:06 -0300

    Preparar conexion PostgreSQL para produccion

--- app/core/database.py
+++ app/core/database.py
@@ -15,9 +15,14 @@ class Base(DeclarativeBase):
 
 # Engine principal de base de datos.
 # Por ahora usamos SQLite local.
+engine_kwargs = {}
+
+if settings.DATABASE_URL.startswith("sqlite"):
+    engine_kwargs["connect_args"] = {"check_same_thread": False}
+
 engine = create_engine(
     settings.DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    **engine_kwargs,
 )
 
 # Fábrica de sesiones.
